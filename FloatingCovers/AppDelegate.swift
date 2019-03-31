81e63b3120b8aee220ca5948aaf26fa43e0e4ef5
Mac Bellingrath <macbellingrath@gmail.com>
2019-03-31 09:49:21 -0400

    [Add] adds initial project

---
+++ FloatingCovers/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FloatingCovers
+//
+//  Created by Mac Bellingrath on 3/31/19.
+//  Copyright © 2019 Mac Bellingrath. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ FloatingCovers/GameViewController.swift
@@ -0,0 +1,92 @@
+//
+//  GameViewController.swift
+//  FloatingCovers
+//
+//  Created by Mac Bellingrath on 3/31/19.
+//  Copyright © 2019 Mac Bellingrath. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    private func setUpScene() {
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/brights.scn")!
+        let scnView = self.view as! SCNView
+        scnView.autoenablesDefaultLighting = false
+
+        // set the scene to the view
+        scnView.scene = scene
+
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+
+        let light = scene.rootNode.childNode(withName: "omni", recursively: true)!
+        scnView.pointOfView = light
+        #if DEBUG
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        #endif
+    }
+
+    override func viewDidLoad() {
+        setUpScene()
+        super.viewDidLoad()
+    }
+    
+    @objc
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.red
+            
+            SCNTransaction.commit()
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+}
